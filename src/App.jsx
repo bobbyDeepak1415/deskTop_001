1fa9a324e91d1584ec98d50c4f2e5a47c030f7ec
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-04 17:07:06 +0530

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react"
 
 function App() {
 
+
+  const [count,setCount]=useState(0)
+
   return (
-  <>
-  hello
-  </>
+  <div>
+  <h2>count at:{count}</h2>
+  <button>-</button>
+  <button>-</button>
+  </div>
   )
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,12 @@
 
 
+import { configureStore } from "@reduxjs/toolkit"
+ 
+
+
+const store=configureStore({
+    counter:"counterReducer"
+})
+
+
+export default store
