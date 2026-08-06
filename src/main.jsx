7652d2887ebd82e5e714c0a065384c74964256aa
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-06 15:09:12 +0530

    setup done

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,17 @@ let arr1=[23,4,[5,6,[7,4,[34,23],90]]]
 
 function display(arr){
 
+
+  let result=[]
+
+  for(let i of arr){
+    if(Array.isArray(i)){
+      result.push(...display(i))
+    }
+
+    result.push(i)
+  }
+
 }
 
 console.log(display(arr1))
